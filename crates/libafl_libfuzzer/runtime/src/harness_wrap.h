22f8b512e5de2af744645d65ce8ae2d710690009
Joe Ranweiler <joranwei@microsoft.com>
2025-12-08 17:02:10 -0800

    Hack in upstream static string

--- crates/libafl_libfuzzer/runtime/src/harness_wrap.h
+++ crates/libafl_libfuzzer/runtime/src/harness_wrap.h
@@ -7,6 +7,8 @@
 extern "C" {
 #endif
 
+volatile const char *__HACK_DO_NOT_MERGE = "ERROR: libFuzzer";
+
 int libafl_libfuzzer_test_one_input(int (*harness)(const uint8_t *, size_t),
                                     const uint8_t *data, size_t len);
 
